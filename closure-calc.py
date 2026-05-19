305679ce4e020e5ff5396763ce8ab2d1b66a7859
gsuofc <113817803+gsuofc@users.noreply.github.com>
2026-05-19 10:29:49 -0600

    Add rounding to DMS feature
    fixes 0-03-60 issue in #1

--- closure-calc.py
+++ closure-calc.py
@@ -831,11 +831,33 @@ class ClosureCalc(tk.Tk):
 
         return (dx,dy)
     
-    def compute_dms_from_dd(self,dd):
+    def compute_dms_from_dd(self,dd, round_off = True):
         b_d = math.floor(dd)
         b_min = (dd-b_d)*60
         b_m = math.floor(b_min)
         b_s = (b_min-b_m)*60
+        if round_off:
+            #check to see if seconds rounds to 60 - if so fix
+            rounded_s = round(b_s, 3)
+            if rounded_s >=60.0:
+                b_m +=1
+                b_s -=60
+                if b_m >= 60.0:
+                    b_d +=1
+                    b_m -=60
+                    if b_d >= 360.0:
+                        b_d-=360
+
+            #given that if we do the above, 59.9999 will become -0.0001. This will round to -0, we would rather just have 0. 
+            if b_d < 0:
+                b_d = 0
+            if b_m < 0:
+                b_m = 0
+            if b_s < 0:
+                b_s = 0
+            #Perhaps there is a better way of doing this - maybe having rounding as arguement rather than being hardcoded? TODO: 
+            #For now adding default argument in case I want to not do this
+
         return (b_d, b_m, b_s)
     
     def compute_dd_from_dms(self,d,m,s):
