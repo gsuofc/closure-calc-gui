695b38fd288937a10f1fcf0bb440ea2d1fc2a09b
gsuofc <113817803+gsuofc@users.noreply.github.com>
2026-05-21 14:09:30 -0600

    Mac Fixes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 here="`dirname \"$0\"`"
 cd "$here" || exit 1
 python3 gen_version_number.py
-python3 -m PyInstaller --onefile -w closure-calc.py --add-data "icon.ico;."
+python3 -m PyInstaller --onefile -w closure-calc.py --add-data "icon.ico:."
